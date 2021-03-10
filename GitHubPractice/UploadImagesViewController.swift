4bbc418a2b84e21d12379504b767becee205cb19
Ravi <ravihepto019@gmail.com>
2021-03-11 00:38:38 +0530

    implemented upload images

---
+++ GitHubPractice/UploadImagesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UploadImagesViewController.swift
+//  GitHubPractice
+//
+//  Created by Apple on 11/03/21.
+//
+
+import UIKit
+
+class UploadImagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GitHubPractice/ViewController.swift
+++ GitHubPractice/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let uploadVc = UploadImagesViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
